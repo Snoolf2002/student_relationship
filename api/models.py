86abed3dc82357b3c45f8cc1c75fbc7d4b214bcb
Snoolf001 <abdurahmonovjaloliddin2002@gmail.com>
2023-03-14 10:39:41 +0500

    created view for get all students

--- api/models.py
+++ api/models.py
@@ -5,8 +5,14 @@ class Contact(models.Model):
     phone = models.CharField(max_length=20)
     adress = models.CharField(max_length=50)
 
+    def __str__(self) -> str:
+        return self.phone
+
 
 class Student(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     contact = models.OneToOneField(Contact, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.first_name
--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpRequest, JsonResponse
+from .models import Student, Contact
 
 # Create your views here.
+
+def get_student(request: HttpRequest) -> JsonResponse:
+    data = Student.objects.all()
+
+    res = []
+    for dat in data:
+        contact = dat.contact
+        res.append(
+            {
+                "firstname": dat.first_name,
+                "lastname": dat.last_name,
+                "phone": contact.phone,
+                "adress": contact.adress
+            }
+        )
+    
+    return JsonResponse({'students': res})
